2f45e96c3f4f452df11527beef81bda71cc58331
laizaraujo8 <laiz.araujo8@gmail.com>
2019-01-28 13:39:23 -0200

    js no arquivo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
-window.cipher = {
-  // ... 
-};
+function proximaLetra(s){
+  var c= s.charCodeAt(0);
+  swith(c){
+   case 126: return " ";
+   default: return String.fromCharCode(++c);
+  }
+ }
+ 
+ function cifrarConteudo (){
+   var valor = $("area_texto").val();
+   var deslocamento = $("deslocamento").val();
+   var valorcifrado = "";
+   for (i=0; i(valor.lenght; i++)){
+     var novoCaractere = String.fromCharCode(valor.charCodeAt(i));
+     for (j=0; j(deslocamento; j++)){
+       novocaractere = proximaLetra(novocaractere);
+     }
+     valorcifrado = valorcifrado + novocaractere;
+   }
+   document.getElementById("area_texto").value = valorcifrado;
