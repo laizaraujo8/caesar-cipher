d8754b622d7f60f1e5192f22249186f86c026a47
laizaraujo8 <laiz.araujo8@gmail.com>
2019-01-30 17:02:30 -0200

    testes

--- src/tests.js
+++ src/tests.js
@@ -1,3 +1,18 @@
+function testes(cipher, texto, chave, resultado) {
+   var meuTest=cipher(texto, chave);
+   if(meuTest=resultado){
+      console.log("Teste satisfatório");
 
+   }else{
+      console.log("Teste falho");
    }
-    
+}
+
+
+testes(encode, "Xuxu", 1, "Yvyv");
+testes(encode, "Baba", 5, "Gfgf");
+testes(encode, "Oi Laiz", 8, "Wq Tiqh");
+
+testes(decode, "Yvyv",1, "Xuxu");
+testes(decode, "Gfgf", 5, "Baba");
+testes(decode, "Wq Tiqh", 8, "Oi Laiz");
