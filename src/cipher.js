201b2e057627319cabbf31dc0054fcb30b8fa16a
laizaraujo8 <laiz.araujo8@gmail.com>
2019-01-30 16:30:47 -0200

    crip e descriptografando

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,18 @@
 
-function criptografar() {
+function criptografar(){
   let inserir1 = document.getElementById("inserirTexto").value;
   let chave1 = parseInt(document.getElementById("inserirChave").value);
   let resultadoCrip = encode(inserir1, chave1);
-
   document.getElementById("colarTextoCrip").value = resultadoCrip;
 }
+
+function descriptografar(){
+  let inserir2 = document.getElementById("colarTextoCrip").value;
+  let chave2 = parseInt(document.getElementById("inserirChave").value);
+  let resultadoDesCrip = decode(inserir2, chave2);
+  document.getElementById("inserirTexto").value = resultadoDesCrip;
+}
+
 
 
 function encode(texto, chave) {
@@ -29,10 +36,42 @@ function encode(texto, chave) {
       novoCodigo = ((code - codPrinLetra + chave) % 26) + codPrinLetra;
     }
 
-    console.log("code: " + code + "=> newCode: " + novoCodigo);
+
 
     strEncoded += String.fromCharCode(novoCodigo);
   }
 
   return strEncoded;
+}
+
+
+
+
+function decode(texto, chave) {
+  let strDecoded = "";
+
+  for (let i = 0; i < texto.length; i++) {
+    let code = texto.charCodeAt(i);
+    let codPrinLetra = 0;
+    let novoCodigo = 0;
+
+    if (code == 32) {
+      novoCodigo = code;
+    }
+    else {
+      if (code >= 65 && code <= 90) {
+        codPrinLetra = 65;
+      }
+      if (code >= 97 && code <= 122) {
+        codPrinLetra = 97;
+      }
+      novoCodigo = ((code - codPrinLetra - chave) % 26) + codPrinLetra;
+    }
+
+
+
+    strDecoded += String.fromCharCode(novoCodigo);
+  }
+
+  return strDecoded;
 }
