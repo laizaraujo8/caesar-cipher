142194e1b61d6a151a8a41c27fff410d9af0a120
laizaraujo8 <laiz.araujo8@gmail.com>
2019-01-31 16:43:29 -0200

    readme e mais testes

--- src/tests.js
+++ src/tests.js
@@ -12,7 +12,13 @@ function testes(cipher, texto, chave, resultado) {
 testes(encode, "Xuxu", 1, "Yvyv");
 testes(encode, "Baba", 5, "Gfgf");
 testes(encode, "Oi Laiz", 8, "Wq Tiqh");
+testes(encode, "abcdefghi", 12, "mnopqrstu");
+testes(encode, "jklmnopqrs", 15, "yzabcdefgh");
+testes(encode, "tuvwxyz", 22, "pqrstuv");
 
 testes(decode, "Yvyv",1, "Xuxu");
 testes(decode, "Gfgf", 5, "Baba");
 testes(decode, "Wq Tiqh", 8, "Oi Laiz");
+testes(decode, "ABCDEFGHI", 12, "MNOPQRSTU");
+testes(decode, "JKLMNOPQRS", 15, "YZABCDEFGH")
+testes(decode, "TUVWXYZ", 22, "PQRSTUV");
